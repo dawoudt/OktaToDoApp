96832ce011bba09689d17a6542055860f1c5b96b
dawoud_t <dtabboush@gmail.com>
2021-10-18 18:17:15 +1100

    Rename helper.py to okta.py

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -3,14 +3,14 @@ from django.contrib.auth import get_user_model
 from rest_framework import authentication
 from rest_framework import exceptions
 
-from api import helpers
+from api import okta
 
 
 class OktaAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         if bearer_token := request.META.get('HTTP_AUTHORIZATION', None):
-            if helpers.is_access_token_valid(bearer_token):
-                jwt = helpers.parse_token(bearer_token)
+            if okta.is_access_token_valid(bearer_token):
+                jwt = okta.parse_token(bearer_token)
                 email = jwt[1]['sub']
                 user, _ = get_user_model().objects.get_or_create(email=email)
                 return (user, None) # authentication successful
